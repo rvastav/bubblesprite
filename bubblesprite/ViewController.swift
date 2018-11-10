18c32e35904f5189f372454c1444978b95a1f5d5
Rishab Srivastav <rvastav.rs@gmail.com>
2018-11-09 20:46:19 -0400

    bubble game using spritekit framework

---
+++ bubblesprite/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  bubbleCollision
+//
+//  Created by Gagan on 2018-10-02.
+//  Copyright © 2018 Gagan. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let scene = BubbleScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+
+}
+
